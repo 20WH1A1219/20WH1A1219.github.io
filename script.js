f0147fafbe9a7e54cc3c6a5645b858595ec2d707
20WH1A1219 <96655092+20WH1A1219@users.noreply.github.com>
2023-06-05 21:41:50 +0530

    Creation of UserCard 2

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
-//alert("Hello!");
-console.log("Hi!!");
+var data1 = {image:"Current.jpg",name:"Nimisha",designation:"Student"}
+var data2 = {image:"Childhood.jpg",name:"Nimmi",designation:"SDE"}
+var isData1 = true;
+var displayData;
+var changeMyPic = function(){
+    if(isData1){
+        displayData = data2;
+        isData1 = false;
+    }
+    else{
+        displayData = data1;
+        isData2 = true;
+    }
+    document.getElementById("myImage").src = displayData.image;
+}
