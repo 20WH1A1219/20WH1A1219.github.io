efeb121d69738139e0c23971f11e94e09ebdda51
20WH1A1219 <96655092+20WH1A1219@users.noreply.github.com>
2023-06-06 21:45:43 +0530

    UserCard 3 creation

---
+++ userCard3.js
@@ -0,0 +1,12 @@
+var userInfo = {name: "",img : "",userName: ""};
+var changeRand = function(){
+    fetch("https://randomuser.me/api").then((response)=>response.json()).then((data)=>
+    {
+        userInfo.name = data.results[0].name.first;
+        userInfo.img = data.results[0].picture.large;
+        userInfo.userName = data.results[0].login.username;
+        document.getElementById("myImage").src = userInfo.img;
+        document.getElementById("myName").innerHTML = userInfo.name;
+        document.getElementById("myUserName").src = userInfo.userName;
+    });   
+}
